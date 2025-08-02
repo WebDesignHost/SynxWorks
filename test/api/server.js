7044d2482dba0d6419c810cf37757d5f3d4228dd
WebDesignHost <amighiaidan@gmail.com>
2025-08-01 23:03:09 -0400

    Update server.js

--- test/api/server.js
+++ test/api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -85,3 +87,4 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`SynxBot (chat-completions) running at http://localhost:${PORT}`);
 });
+
